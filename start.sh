d660d5555c5ded8aab3b268c255553471882f035
cavilosa <cavilosa33@gmail.com>
2025-09-10 14:15:41 -0700

    Fix: added port default value to the start.sh.

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 
 # The --bind 0.0.0.0:5000 tells Gunicorn to listen on all network interfaces.
 # The 'run:app' tells Gunicorn to look in the 'run.py' file for the 'app' variable.
-gunicorn --workers 3 --bind "0.0.0.0:${PORT}" run:app
+gunicorn --workers 3 --bind "0.0.0.0:${PORT:-8080}" run:app
